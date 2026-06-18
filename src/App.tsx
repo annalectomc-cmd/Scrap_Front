fbf769335227ae44a96b369f79ea3922be4691eb
Daniel Rodriguez Fuentes <annalectomc@gmail.com>
2026-06-18 08:38:13 -0500

    bootstrpap implementing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
@@ -8,9 +8,9 @@ function App() {
   return (
     <>
     <div>
-    <SearchForm setComments={setComments}/>
+      <SearchForm setComments={setComments}/>
     </div>
-    <div>
+    <div className="container mt-4">
       <TableDown comments={comments} />
     </div>
     </>
@@ -27,6 +27,7 @@ function SearchForm({setComments}: any){
     .then((response) => response.json())
     .then((data) => {
       setComments(data);
+      
     })
     .catch((err) => {
       console.log(err.message);
@@ -34,14 +35,13 @@ function SearchForm({setComments}: any){
   }
 
   return(
-    <div>
-        <form>
-          <div>
-            <input type="text" placeholder="perfil de TikTok" value={profile} 
-            onChange={(e) => setProfile(e.target.value)}/>
+    <div className="container mt-4">
+        <form className="row g-3">
+          <div className="col-md-6">
+            <input type="text" placeholder="perfil de TikTok" value={profile} onChange={(e) => setProfile(e.target.value)}/>
           </div>
-          <div>            
-            <button type="button" onClick={sendProfile} >
+          <div className="col-12">            
+            <button type="button" className="btn btn-primary" onClick={sendProfile} >
               Scrapp
             </button>    
           </div>
@@ -91,12 +91,12 @@ function TableDown({comments}: { comments: Comment[] }){
 
   return(
     <>
-    <div>
-      <button type="button" onClick={downloadJson}>
-        Descargar Json
+    <div className="btn-group" role="group">
+      <button type="button" className="btn btn-light" onClick={downloadJson}>
+        <i className="bi bi-filetype-json"></i>
       </button>
-      <button type="button" onClick={downloadCSV}>
-        Descargar CSV
+      <button type="button" className="btn btn-light" onClick={downloadCSV}>
+        <i className="bi bi-filetype-csv"></i>
       </button>
     </div>
     <div>
@@ -108,6 +108,12 @@ function TableDown({comments}: { comments: Comment[] }){
 
 function Table({comments}: { comments: Comment[] }){
 
+  const [paginaActual, setPaginaActual] = useState(1);
+  const porPagina = 10;
+  const totalPaginas = Math.ceil(comments.length / porPagina);
+  const inicio = (paginaActual - 1) * porPagina;
+  const filasVisibles = comments.slice(inicio, inicio + porPagina);
+
   const rows: any = []
   comments.forEach((element) => {
     rows.push(
@@ -116,7 +122,8 @@ function Table({comments}: { comments: Comment[] }){
   });
 
   return(
-    <table>
+    <div className="container mt-4"> 
+      <table className="table table-bordered">
           <thead>
             <tr>
               <th>Id video</th>
@@ -125,9 +132,40 @@ function Table({comments}: { comments: Comment[] }){
             </tr>
           </thead>
           <tbody>
-            {rows}
+            {filasVisibles.map((c, i) => (
+            <tr key={inicio + i}>
+              <td>{c.video_id}</td>
+              <td>{c.user}</td>
+              <td>{c.comment}</td>
+            </tr>
+          ))}
           </tbody>
-        </table>
+      </table>
+      <nav>
+        <ul className="pagination justify-content-center">
+          <li className={`page-item ${paginaActual === 1 ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => setPaginaActual(p => p - 1)}>
+              Anterior
+            </button>
+          </li>
+
+          {Array.from({ length: totalPaginas }, (_, i) => i + 1).map(num => (
+            <li key={num} className={`page-item ${paginaActual === num ? 'active' : ''}`}>
+              <button className="page-link" onClick={() => setPaginaActual(num)}>
+                {num}
+              </button>
+            </li>
+          ))}
+
+          <li className={`page-item ${paginaActual === totalPaginas ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => setPaginaActual(p => p + 1)}>
+              Siguiente
+            </button>
+          </li>
+        </ul>
+      </nav>
+    
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
 import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <nav className="navbar bg-body-tertiary">
+        <header className="brand-name">
+          <h1>Scrapping TikTok</h1>
+        </header>
+    </nav>
     <App />
   </StrictMode>,
 )
