0159ca63130ee1e8505bc7e89c1dc6acc8421577
Nicolasneira10 <nicolas.neira@omnicommediagroup.com.co>
2026-09-24 11:00:22 -0500

    feat(theme): implementar contexto y toggle de tema claro/oscuro.

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -2,75 +2,52 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import NotificationBell from "../features/notifications/components/NotificationBell";
 import "../layout/Navbar.css";
+import { ThemeToggle } from '../features/theme/components/ThemeToggle';
 
 export default function Navbar() {
-
     const [menuOpen, setMenuOpen] = useState(false);
-
     const navigate = useNavigate();
 
     const logout = () => {
-
         // Aquí después eliminaremos el token
-
         navigate("/");
-
     };
 
     return (
-
         <header className="navbar">
-
             <div></div>
 
             <div className="navbar-right">
 
-                {/* NOTIFICACIONES */}
+                <ThemeToggle />
 
                 <NotificationBell />
 
                 <div className="profile-container">
-
                     <button
                         className="profile-avatar"
                         onClick={() => setMenuOpen(!menuOpen)}
                     >
-
                         U
-
                     </button>
 
                     {menuOpen && (
-
                         <div className="profile-menu">
-
                             <div className="profile-name">
-
                                 Usuario
-
                             </div>
-
                             <button
                                 className="logout-button"
                                 onClick={logout}
                             >
-
                                 <i className="bi bi-box-arrow-right"></i>
-
                                 Cerrar sesión
-
                             </button>
-
                         </div>
-
                     )}
-
                 </div>
 
             </div>
-
         </header>
-
     );
-
 }
